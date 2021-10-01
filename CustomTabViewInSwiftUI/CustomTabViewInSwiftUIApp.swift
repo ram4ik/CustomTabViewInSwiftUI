87178a2d939c2137e09c2317a5f19b7a3311a46e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-01 20:59:15 +0300

    Initial Commit

---
+++ CustomTabViewInSwiftUI/CustomTabViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewInSwiftUIApp.swift
+//  CustomTabViewInSwiftUI
+//
+//  Created by ramil on 01.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
